64884f9ef18a574cba08f10d16df53d776358c58
VladislavChernyshev <v.chernyshev911@gmail.com>
2022-08-29 21:40:24 +1000

    homework lesson 5

---
+++ lesson5-hibernate/src/main/java/ru/chernyshev/model/homework/Product.java
@@ -0,0 +1,32 @@
+package ru.chernyshev.model.homework;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private Double cost;
+
+
+    public Product(String title, Double cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+}
