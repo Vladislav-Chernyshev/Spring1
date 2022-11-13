583fe551898272bef1cce035bc0d2ad383f4fc45
VladislavChernyshev <v.chernyshev911@gmail.com>
2022-11-13 15:20:29 +1000

    Урок 11. Добавили авторизацию

--- lesson4-spring-boot/src/main/java/ru/chernyshev/service/UserService.java
+++ lesson4-spring-boot/src/main/java/ru/chernyshev/service/UserService.java
@@ -1,10 +1,12 @@
 package ru.chernyshev.service;
 
-import com.querydsl.core.BooleanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.chernyshev.model.QUser;
 import ru.chernyshev.model.User;
@@ -12,6 +14,8 @@ import ru.chernyshev.model.dto.UserDto;
 import ru.chernyshev.model.mapper.UserDtoMapper;
 import ru.chernyshev.repository.UserRepository;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,6 +28,7 @@ public class UserService {
 
     private final UserDtoMapper mapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
     public Page<UserDto> findAllByFilter(String usernameFilter, String emailFilter, int page, int size, String sortField) {
         usernameFilter = usernameFilter == null || usernameFilter.isBlank() ? null : "%" + usernameFilter.trim() + "%";
@@ -37,11 +42,20 @@ public class UserService {
     }
 
     public void save(UserDto dto) {
-        userRepository.save(mapper.map(dto));
+        userRepository.save(mapper.map(dto, encoder));
     }
 
     public void deleteUserById(Long id) {
         userRepository.deleteById(id);
+    }
+
+    public org.springframework.security.core.userdetails.User findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(user -> new org.springframework.security.core.userdetails.User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        Collections.singletonList(new SimpleGrantedAuthority("ADMIN"))
+                )).orElseThrow(() -> new UsernameNotFoundException(username));
     }
 
 }
